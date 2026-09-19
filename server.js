c342a4ca86ba323b1183ca65c2f53635cbce6f60
Kwincy Shah <8151shahkwincy@gmail.com>
2026-09-20 02:04:35 +0530

    update cors settings

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const User = require('./User');
 const keypoints = require('./keypoints');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
